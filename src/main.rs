95e19a93b87708c5b8cd94adc2468521b9502f72
Arun-2010 <arunbandari15@gmail.com>
2026-06-12 22:37:18 +0530

    bool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+/* 
 fn main() {
-    println!("Hello, world!");
+    let ans: u32 = sum(1, 2);
+    println!("The sum is {}", ans);
 }
+
+fn sum(a: u32, b: u32) -> u32 {
+    return a + b;
+}
+*/
+
+
+
+// boolean
+
+fn main(){
+    print!("{}",is_even(5));
+}
+
+fn is_even(n:u32)->bool{
+    return n%2 == 0;
+}
+
